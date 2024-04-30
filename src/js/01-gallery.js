6206f465747ead40e5e008d072c3059540a7e894
GosiaDabrowska <d.dabrowska.m@gmail.com>
2024-04-30 02:43:38 +0200

    npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+
+function createGalleryItem(image) {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        data-source="${image.original}"
+        alt="${image.description}"
+      />
+    </a>
+  </li>`
+} 
+
+const galleryString = galleryItems.map(createGalleryItem).join("")
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryString);
+
+
+galleryContainer.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const lgImg = ev.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${lgImg}" width="800" height="600">
+`)
+
+instance.show()   
+
+const closeLightboxOnEscape = (ev) => {
+  if(ev.key === 'Escape') {
+    instance.close();
+    document.removeEventListener("keydown", closeLightboxOnEscape)
+  }
+}
+document.addEventListener("keydown", closeLightboxOnEscape)
+});
